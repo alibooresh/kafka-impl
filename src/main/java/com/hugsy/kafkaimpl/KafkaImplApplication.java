982b8e1aef8ff5861b4a325fd63ceb9e15e92d41
Ali Booresh <alibooresh1936@gmail.com>
2023-05-08 19:10:04 +0430

    Initial commit

---
+++ src/main/java/com/hugsy/kafkaimpl/KafkaImplApplication.java
@@ -0,0 +1,13 @@
+package com.hugsy.kafkaimpl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaImplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaImplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hugsy/kafkaimpl/KafkaImplApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugsy.kafkaimpl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaImplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
